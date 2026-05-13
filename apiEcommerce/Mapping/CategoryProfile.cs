114678ce41da142b1a9019afb3b9c8e8f72946dd
Yoval128 <yovalaviles812@gmail.com>
2026-05-12 18:19:24 -0600

    feat; Mapping

---
+++ apiEcommerce/Mapping/CategoryProfile.cs
@@ -0,0 +1,16 @@
+﻿using apiEcommerce.Models;
+using apiEcommerce.Models.Dtos;
+using AutoMapper;
+
+namespace apiEcommerce.Mapping
+{
+    public class CategoryProfile : Profile
+    {
+        public CategoryProfile()
+        {
+            CreateMap<Category, CategoryDto>().ReverseMap();
+
+            CreateMap<Category, CreateCategoryDto>().ReverseMap();
+        }
+    }
+}
