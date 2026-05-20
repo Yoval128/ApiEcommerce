508110adf52f7a224fedd03c5e44b00b1f805e17
Yoval128 <yovalaviles812@gmail.com>
2026-05-19 18:36:09 -0600

    feat: ProductController

---
+++ apiEcommerce/Controllers/ProductsController.cs
@@ -0,0 +1,25 @@
+﻿using apiEcommerce.Repository.IRepository;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+
+namespace apiEcommerce.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+
+        private readonly IProductRepository _productRepository;
+
+        private readonly IMapper _mapper;
+
+        public ProductsController(IProductRepository productRepository, IMapper mapper)
+        {
+            _productRepository = productRepository;
+
+            _mapper = mapper;
+
+        }
+
+    }
+}
