d12ba53dc153d8a8ee5abaa176b9b516b0cfd5d0
Yoval128 <yovalaviles812@gmail.com>
2026-07-12 12:00:55 -0600

    feat:User

--- apiEcommerce/Models/User.cs
+++ apiEcommerce/Models/User.cs
@@ -1,6 +1,14 @@
-﻿namespace apiEcommerce.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace apiEcommerce.Models
 {
     public class User
     {
+        [Key]
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Username { get; set; }
+        public string? Password { get; set; }
+        public string? Role { get; set; }
     }
 }
